76e7a643da4022d4a882171b31c99824a26801a6
buiquoccuongk8lk <167599592+buiquoccuongk8lk@users.noreply.github.com>
2025-12-18 22:05:53 +0700

    Add files via upload

--- temp.cpp
+++ temp.cpp
@@ -1,4 +1,113 @@
-/* CHT_MAX
+/*
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+template <class A, class B>
+    bool maximize(A &a, const B b)
+    {
+        if (a < b)
+        {
+            a = b;
+            return true;
+        } return false;
+    }
+
+template <class A, class B>
+    bool minimize(A &a, const B b)
+    {
+        if(a > b)
+        {
+            a = b;
+            return true;
+        } return false;
+    }
+
+using pII = pair <int, int>;
+using vI = vector <int>;
+using vL = vector <long long>;
+using pLI = pair <long long, int>;
+
+#define BIT(mask, i) ((mask >> (i)) & 1)
+#define MASK(a) (1LL<<(a))
+#define FOR(i, a, b) for(int i = a; i <= (int)b; i++)
+#define FORD(i, a, b) for(int i = a; i >= (int)b; i--)
+#define fi first
+#define se second
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define sz(a) (int)a.size()
+
+const int mod = 1e9 + 7;
+const int maxn = 2e5 + 5;
+
+void process()
+{
+
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    #define taskname "kieuoanh"
+    if(fopen(taskname".inp", "r"))
+    {
+        freopen(taskname".inp", "r", stdin);
+        freopen(taskname".out", "w", stdout);
+    }
+
+    int tc = 1; /// cin >> tc;
+    while (tc--) process();
+
+    return 0;
+}
+*/
+/*
+#include <bits/stdc++.h>
+#define ALL(A) (A).begin(), (A).end()
+#define TIME  (1.0 * clock() / CLOCKS_PER_SEC)
+#define file(Roxy) if(fopen(Roxy".inp", "r")){freopen(Roxy".inp", "r", stdin);freopen(Roxy".out", "w", stdout);}
+#define fi first
+#define se second
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FOD(i, a, b) for (int i = (a); i >= (b); i--)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+const string NAME = "";
+const int TEST = 100;
+
+mt19937 rd(time(0));
+
+int Rand(int l, int r) {
+    return l + rd()%(r - l + 1);
+}
+
+void create() {
+    ofstream inp((NAME + ".inp").c_str());
+
+    inp.close();
+}
+
+signed main() {
+    srand(time(NULL));
+    for(int i = 1; i <= TEST; i++) {
+        create();
+        system((NAME + "_trau.exe").c_str());
+        system((NAME + ".exe").c_str());
+        if(system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0) {
+            cout << "WRONG !!";
+            return 0;
+        }
+        cout << "CORRECT\n";
+    }
+    return 0;
+}
+*/
+/*
 struct Line {
     ll a, b;
     Line (ll a, ll b) : a(a), b(b) {}
@@ -37,8 +146,7 @@ struct CHT {
     }
 };
 */
-
-/*  chtmin
+/*
 struct Line {
     ll a, b;
 
@@ -80,7 +188,7 @@ struct CHT {
     }
 };
 */
-/* kmp
+/*
 void build_kmp(void) {
     int k = kmp[1] = 0;
     for (int i = 2; i <= m; i++) {
@@ -137,8 +245,10 @@ void build_hash(){
 }
 */
 /*
+
 struct Points {
     ll x, y;
+
 
         A = y2 - y1;
         B = x1 - x2
@@ -160,9 +270,7 @@ struct Points {
         }
 
 } P[maxn];
-/*
 
-/*
 ll ccw(Points a, Points b, Points c) {
     return (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
 }
@@ -212,143 +320,150 @@ bool intersect(Points A, Points B, Points C, Points D) {
     return (ABxAC * ABxAD < 0 && CDxCA * CDxCB < 0);
 }
 */
-
 /*
-struct Node{
-	Node *link, *go[26], *child[26];
-	vector <Node*> g;
-	Node(){
-		link = NULL;
-		g.clear();
-		FOR(i, 0, 25) go[i] = child[i] = NULL;
+struct Matrix {
+    #define N_Matrix 5
+    ll x[N_Matrix][N_Matrix];
+
+    Matrix() {} ;
+
+    Matrix(ll a[N_Matrix][N_Matrix]) {
+        FOR(i, 1, N_Matrix) {
+            FOR(j, 1, N_Matrix) {
+                x[i][j] = a[i][j];
+            }
+        }
+    }
+
+    Matrix operator *(const Matrix &b) {
+        Matrix c;
+        FOR(i, 1, N_Matrix) {
+            FOR(j, 1, N_Matrix) {
+                c.x[i][j] = 0;
+                FOR(t, 1, N_Matrix) {
+                    c.x[i][j] = (c.x[i][j] + x[i][t] * b.x[t][j] % sm) % sm;
+                }
+            }
+        }
+        return c;
+    }
+
+    friend Matrix operator ^(const Matrix a, const ll b) {
+        if(b == 1) return a;
+        Matrix c = a ^ (b / 2);
+        if(b & 1) return c * c * a;
+        else return c * c;
+    }
+};
+*/
+/*
+struct Line {
+	ll a, b;
+
+	Line() {
+		a = 0;
+		b = - 1e18;
+	};
+
+	ll compute(ll x) {
+		return 1ll * a * x + b;
 	}
 };
-Node *root = new Node();
-inline void addString(const string &s){
-	Node *p = root;
-	for(const char &c : s){
-		int j = c - 'a';
-		if(p -> child[j] == NULL) p -> child[j] = new Node();
-		p = p -> child[j];
+
+struct LCT_MAX {
+	Line st[4 * N];
+
+	void update(int id, int l, int r, Line L) {
+		if(l == r) {
+			if(st[id].compute(l) < L.compute(l)) swap(st[id], L);
+			return;
+		}
+		int mid = (l + r) >> 1;
+		if(st[id].a > L.a) swap(st[id], L);
+		if(st[id].compute(mid) < L.compute(mid)) {
+			swap(st[id], L);
+			update(id << 1, l, mid, L);
+		} else update(id << 1 | 1, mid + 1, r, L);
 	}
-}
-void AC_BFS(void){
-    root -> link = root;
-    FOR(i, 0, 25) root -> go[i] = root -> child[i] != NULL ? root -> child[i] : root;
-    queue <Node*> que;
-    que.push(root);
-    while(!que.empty()){
-        Node *p = que.front(); que.pop();
-        FOR(i, 0, 25) if(p -> child[i] != NULL){
-            Node *q = p -> child[i];
-            q -> link = p == root ? p : p -> link -> go[i];
-            q -> link -> g.push_back(q);
-            FOR(j, 0, 25) q -> go[j] = q -> child[j] != NULL ? q -> child[j] : q -> link -> go[j];
-            que.push(q);
-        }
-    }
-}
+
+	ll get(int id, int l, int r, int x) {
+		if(l == r) return st[id].compute(x);
+		int mid = (l + r) >> 1;
+		if(x <= mid) return max(get(id << 1, l, mid, x), st[id].compute(x));
+		else return max(get(id << 1 | 1, mid + 1, r, x), st[id].compute(x));
+	}
+};
 */
 /*
-#include <bits/stdc++.h>
-#define ALL(A) (A).begin(), (A).end()
-#define TIME  (1.0 * clock() / CLOCKS_PER_SEC)
-#define file(Roxy) if(fopen(Roxy".inp", "r")){freopen(Roxy".inp", "r", stdin);freopen(Roxy".out", "w", stdout);}
-#define fi first
-#define se second
-#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
-#define FOD(i, a, b) for (int i = (a); i >= (b); i--)
-#define REP(i, n) for (int i = 0; i < (n); i++)
-using namespace std;
+struct FT {
+    ll bit[N];
 
-const int MAXN = 2e5 + 5;
-const string NAME = "";
-const int TEST = 100;
-
-mt19937 rd(time(0));
-
-int Rand(int l, int r) {
-    return l + rd()%(r - l + 1);
-}
-
-void create() {
-    ofstream inp((NAME + ".inp").c_str());
-
-    inp.close();
-}
-
-signed main() {
-    srand(time(NULL));
-    for(int i = 1; i <= TEST; i++) {
-        create();
-        system((NAME + "_trau.exe").c_str());
-        system((NAME + ".exe").c_str());
-        if(system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0) {
-            cout << "WRONG !!";
-            return 0;
-        }
-        cout << "CORRECT\n";
+    void up(int u, int val) {
+        for(int i = u; i <= n; i+=i&-i) bit[i]+= val;
     }
-    return 0;
-}
+
+    ll get(int u) {
+        ll sum = 0;
+        for(int i = u; i; i-=i&-i) sum+= bit[i];
+        return sum;
+    }
+
+    ll get(int l, int r) {
+        return get(r) - get(l - 1);
+    }
+
+    /// Range up and Range query
+    // ll bit1[N], bit2[N];
+
+    // void upPoint(int u, ll val, ll bit[]) {
+    //     for(int i = u; i <= n; i+=i&-i) bit[i]+= val;
+    // }
+
+    // ll getPoint(int u, ll bit[]) {
+    //     ll sum = 0;
+    //     for(int i = u; i; i-=i&-i) sum+= bit[i];
+    //     return sum;
+    // }
+
+    // void upRange(int l, int r, int x) {
+    //     upPoint(l, x, bit1);
+    //     upPoint(r + 1, - x, bit1);
+    //     upPoint(l, 1ll * x * (l - 1), bit2);
+    //     upPoint(r + 1, - 1ll * x * r, bit2);
+    // }
+
+    // ll prefix_sum(int u) {
+    //     return 1ll * getPoint(u, bit1) * u - getPoint(u, bit2);
+    // }
+
+    // ll range_sum(int l, int r) {
+    //     return prefix_sum(r) - prefix_sum(l - 1);
+    // }
+};
 */
 /*
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-template <class A, class B>
-    bool maximize(A &a, const B b) {
-        if (a < b) {
-            a = b;
-            return true;
-        } return false;
+int gcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
     }
-
-template <class A, class B>
-    bool minimize(A &a, const B b) {
-        if(a > b) {
-            a = b;
-            return true;
-        } return false;
-    }
-
-using pII = pair <int, int>;
-using vI = vector <int>;
-using vL = vector <long long>;
-using pLI = pair <long long, int>;
-
-#define BIT(mask, i) ((mask >> (i)) & 1)
-#define MASK(a) (1LL<<(a))
-#define FOR(i, a, b) for(int i = a; i <= (int)b; i++)
-#define FORD(i, a, b) for(int i = a; i >= (int)b; i--)
-#define fi first
-#define se second
-#define pb push_back
-#define all(a) a.begin(), a.end()
-#define sz(a) (int)a.size()
-
-const int mod = 1e9 + 7;
-const int maxn = 2e5 + 5;
-
-void process() {
-
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    #define taskname "kieuoanh"
-    if(fopen(taskname".inp", "r")) {
-        freopen(taskname".inp", "r", stdin);
-        freopen(taskname".out", "w", stdout);
-    }
-
-    int tc = 1; /// cin >> tc;
-    while (tc--) process();
-
-    return 0;
+bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g)  {
+    g = gcd(abs(a), abs(b), x0, y0);
+    if(c % g) return 0;
+    x0 *= c / g;
+    y0 *= c / g;
+    if (a < 0) x0 = -x0;
+    if (b < 0) y0 = -y0;
+    return 1;
 }
 */
+
 
